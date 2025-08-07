89d3335506a75b5da6bc20a8b9c0ef6aa123de07
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-08-07 23:04:52 +0100

    remove navItems and add servers

--- www/src/config/site.ts
+++ www/src/config/site.ts
@@ -3,33 +3,14 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Scout",
   description: "Scout analytics dashboard.",
-  navItems: [
+  servers: [
     {
-      label: "Home",
-      href: "/",
+      label: "PvPWars",
+      addr: "pvpwars.net",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "Map",
-      href: "/map",
+      label: "SimplyVanilla",
+      addr: "simplyvanilla.net",
     },
   ],
-  links: {
-    github: "https://github.com/frontio-ai/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
