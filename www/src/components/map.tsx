7fc366d9b7a644f9a78286c87cdc9c6b37890b00
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-08-07 23:03:28 +0100

    update map and add scaling with nearest-neighbor

--- www/src/components/map.tsx
+++ www/src/components/map.tsx
@@ -3,13 +3,31 @@ import L from 'leaflet';
 
 export default function Map() {
     useEffect(() => {
-        const map = L.map('map').setView([51.505, -0.09], 12, {
+        const map = L.map('map', {
+            crs: L.CRS.Simple,
+            center: [0, 0],
+            attributionControl: false,
+            markerZoomAnimation: true,
+            doubleClickZoom: false,
+            zoomControl: false,
+        }).setView([0, 0], 3, {
             animate: true,
         });
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 12,
-            minZoom: 2,
-            tileSize: 256,
+
+        L.tileLayer('https://squaremap-demo.jpenilla.xyz/tiles/world/{z}/{x}_{y}.png', {
+            maxNativeZoom: 3,
+            minNativeZoom: 0,
+            maxZoom: (navigator.maxTouchPoints || 'ontouchstart' in document.documentElement) ? 3 : 6,
+            minZoom: 0,
+            tileSize: 512,
+            detectRetina: true,
+            className: "rendering-pixelated rendering-crisp-edges"
+        }).addTo(map);
+
+        L.marker([0, 0], {
+            icon: L.icon({
+                iconUrl: "/images/icon/spawn.png"
+            })
         }).addTo(map);
 
         return () => {
