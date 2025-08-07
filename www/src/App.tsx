158d1459c49c2aeac4df680bff4ea7110bf5789b
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-08-07 23:02:04 +0100

    update routes

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import MapPage from "@/pages/map";
+import ServerPage from "./pages/server";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<MapPage />} path="/map" />
+      <Route element={<ServerPage />} path="/server/*" />
     </Routes>
   );
 }
